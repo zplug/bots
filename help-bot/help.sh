1d38a2b3791755e116725e5bf08d78ef468b836b
b4b4r07 <b4b4r07@gmail.com>
2016-09-27 00:00:25 +0900

    Add security-guard-bot

--- help-bot/help.sh
+++ help-bot/help.sh
@@ -5,14 +5,19 @@ dir="$(cd $(dirname $0)/..; pwd)"
 printf '['
 for js in "$dir"/*-bot/index.js
 do
-    file=$(echo $js | sed 's!^.*bots/!!;s!/index.js$!!')
-    printf '{'
-    printf '"title":"%s",' "$file"
-    printf '"value":"%s",' "$(
+    file="$(echo "$js" | sed 's!^.*bots/!!;s!/index.js$!!')"
+    help="$(
     cat "$js" \
         | grep -A 1 "Help:$" \
         | tail -1
     )"
+    if [[ -z $help ]]; then
+        continue
+    fi
+
+    printf '{'
+    printf '"title":"%s",' "$file"
+    printf '"value":"%s",' "$help"
     printf '"short":false'
     printf '},'
 done | sed 's/,$//'
