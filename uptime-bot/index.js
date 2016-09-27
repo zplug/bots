27a9a07bafee6a1197d084c37dba942ad2f971da
b4b4r07 <b4b4r07@gmail.com>
2016-09-28 01:41:50 +0900

    Add uptime-bot

---
+++ uptime-bot/index.js
@@ -0,0 +1,34 @@
+/*
+ * Help:
+ * :bar_chart: bot uptime
+ */
+
+var botkit = require('botkit');
+var exec = require('child_process').exec;
+
+var SLACK_TOKEN = process.env.SLACK_TOKEN;
+
+var controller = botkit.slackbot({
+    debug: false
+});
+
+controller.spawn({
+    token: SLACK_TOKEN
+}).startRTM();
+
+controller.hears(['^bot\\s+uptime'],
+        ['message_received', 'ambient'],
+        function(bot, message) {
+            exec(__dirname + '/uptime.sh', function(err, stdout, stderr){
+                if (err) { console.log(err); }
+                var fields = JSON.parse(stdout);
+                var attachments = [{
+                    fields: fields,
+                }]
+                return bot.reply(message, {
+                    attachments: attachments,
+                    icon_emoji: ':bar_chart:',
+                    username: 'uptime bot',
+                })
+            });
+        })
---
+++ uptime-bot/uptime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir="$(cd $(dirname $0)/..; pwd)"
+
+IFS=$'\n'
+printf '['
+for bot in $($dir/node_modules/.bin/forever list | sed '1,2d' | awk '{print $5, $9}')
+do
+    printf '{'
+    printf '"title":"%s",' "$(echo "$bot" | awk '{print $1}')"
+    printf '"value":"%s",' "$(echo "$bot" | awk '{print $2}')"
+    printf '"short":false'
+    printf '},'
+done | sed 's/,$//'
+printf ']\n'
