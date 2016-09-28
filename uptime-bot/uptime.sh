5a4820a16af3d45898d4aaf85bff81be5c98fa1a
b4b4r07 <b4b4r07@gmail.com>
2016-09-28 21:53:29 +0900

    Improve uptime-bot

--- uptime-bot/uptime.sh
+++ uptime-bot/uptime.sh
@@ -7,7 +7,7 @@ printf '['
 for bot in $($dir/node_modules/.bin/forever list | sed '1,2d' | awk '{print $5, $9}')
 do
     printf '{'
-    printf '"title":"%s",' "$(echo "$bot" | awk '{print $1}' | xargs dirname)"
+    printf '"title":"%s",' "$(echo "$bot" | awk '{print $1}' | xargs dirname | xargs basename)"
     printf '"value":"%s",' "$(echo "$bot" | awk '{print $2}')"
     printf '"short":false'
     printf '},'
