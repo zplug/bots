b565c09a894a1cc18f095bfa00cab043e03088d6
b4b4r07 <b4b4r07@gmail.com>
2016-10-03 01:27:22 +0900

    Improve help-bot

--- help-bot/index.js
+++ help-bot/index.js
@@ -27,6 +27,7 @@ var format = function(args) {
     }
 }
 
+/*
 controller.hears(['^bot\\s+help(\\s+(\\S+))?'],
         ['message_received', 'ambient'],
         function(bot, message) {
@@ -38,11 +39,54 @@ controller.hears(['^bot\\s+help(\\s+(\\S+))?'],
                         color: '#ff0000',
                     }));
                 }
-                //var fields = JSON.parse(stdout);
-                //var attachments = [{
-                //    fields: fields,
-                //}]
                 var attachments = JSON.parse(stdout);
+                return bot.reply(message, {
+                    attachments: attachments,
+                    icon_emoji: ':question:',
+                    username: 'help bot',
+                });
+            });
+        });
+        */
+
+controller.hears(['^bot\\s+(help|usage)(\\s+(\\S+))?'],
+        ['message_received', 'ambient'],
+        function(bot, message) {
+            var name = message.match[2];
+            if (typeof name != "undefined") {
+                var attachments = require(__dirname + '/../' + name.trim() + '/usage.json');
+                return bot.reply(message, {
+                    attachments: attachments,
+                    icon_emoji: ':question:',
+                    username: 'help bot',
+                });
+            }
+
+            var fields = [];
+            var Glob = require("glob").Glob;
+            var mg = new Glob(__dirname + "/../*-bot/usage.json", {mark: true}, function(err, matches) {
+                if (err) {
+                    return bot.reply(message, format({
+                        text: stderr,
+                        color: '#ff0000',
+                    }));
+                }
+                matches.forEach(function(match, i) {
+                    var titles = [];
+                    var usage = require(match);
+                    usage[0].fields.forEach(function(f, _) {
+                        titles.push(f.title);
+                    });
+                    fields[i] = {
+                        'title': usage[0].author_name,
+                        'value': titles.join('\n'),
+                        'short': false,
+                    };
+                });
+                var attachments = [{
+                    pretext: 'For more info, type "bot help xxx-bot" to show usage!',
+                    fields: fields,
+                }];
                 return bot.reply(message, {
                     attachments: attachments,
                     icon_emoji: ':question:',
