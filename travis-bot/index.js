8f5df6bda9984f6ae417e8898aaf5493456424e8
b4b4r07 <b4b4r07@gmail.com>
2016-10-05 01:07:50 +0900

    Improve custom subcommand in travis-bot

--- travis-bot/index.js
+++ travis-bot/index.js
@@ -1,6 +1,8 @@
 var botkit = require('botkit');
 var exec = require('child_process').exec;
 var request = require('request');
+var Promise = require('bluebird')
+var sprintf = require('sprintf')
 
 var SLACK_TOKEN = process.env.SLACK_TOKEN;
 var TRAVIS_CI_TOKEN = process.env.TRAVIS_TOKEN;
@@ -39,19 +41,61 @@ var format = function(args) {
     };
 };
 
-controller.hears(['^bot\\s+travis\\s+build'],
-        ['message_received', 'ambient'],
-        function(bot, message) {
-            bot.reply(message, format({
-                text: 'Start to build...',
+request._xxxAsync = function(method, param) {
+    return function(param) {
+        return new Promise(function(resolve, reject) {
+            request[method](param, function(err, response, body) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve({
+                    response: response,
+                    body: body,
+                });
+            });
+        });
+    };
+};
+
+request.getAsync = request._xxxAsync('get')
+request.postAsync = request._xxxAsync('post')
+
+var commandProcMap = {
+    '': function(bot, message) {
+        return bot.reply(message, format({
+            text: 'too few arguments',
+            color: '#ff0000',
+        }));
+    },
+    help: function(bot, message) {
+        var attachments = require(__dirname + "/usage.json");
+        return bot.reply(message, {
+            attachments: attachments,
+            icon_emoji: ':construction_worker:',
+            username: 'travis bot',
+        });
+    },
+    build: function(bot, message) {
+        request.getAsync(options)
+        .then(function() {
+            return bot.reply(message, format({
+                text: 'Called Travis API',
                 color: '#00ff00',
             }));
-            request(options, function (error, response, body) {
-                if (error) {
-                    return bot.reply(message, format({
-                        text: 'Failed to build',
-                        color: '#ff0000',
-                    }));
-                }
-            });
+        });
+    }
+};
+
+controller.hears([/^bot\s+travis(?:\s+(\w+)(?:\s+(\S+))?)?\s*$/],
+        ['message_received', 'ambient'],
+        function(bot, message) {
+            var command = message.match[1] || '';
+            var commandProc = commandProcMap[command];
+            if(! commandProc) {
+                return bot.reply(message, format({
+                    text: sprintf('Unknown command [%s]', command),
+                    color: '#ff0000',
+                }));
+            }
+            commandProc(bot, message);
         });
